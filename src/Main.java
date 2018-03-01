bc4a571acde1f035b367ae103b21f3498985f105
John Hany <johnhany97@gmail.com>
2018-03-01 18:15:52 +0000

    Read all rides

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
@@ -11,8 +12,7 @@ public class Main {
 	int rides = 0;
 	int bonusPerRide = 0;
 	int numberOfSteps = 0;
-	
-    //If we need to read from a file
+	ArrayList<Ride> ridesList = new ArrayList<Ride>();
     BufferedReader br = null;
 	try {
 		br = new BufferedReader(new FileReader("a_example.in"));
@@ -24,6 +24,11 @@ public class Main {
         rides = Integer.parseInt(splited[3]);
         bonusPerRide = Integer.parseInt(splited[4]); //If you start trip on time
         numberOfSteps = Integer.parseInt(splited[5]);
+        for (int i = 0; i < rides; i++) {
+        	line = br.readLine();
+        	splited = line.split(" ");
+        	ridesList.add(new Ride(splited[0], splited[1], splited[2], splited[3], splited[4], splited[5]));
+        }
     }  catch (FileNotFoundException e) {
 		e.printStackTrace();
 	} catch (IOException e) {
@@ -35,11 +40,5 @@ public class Main {
 			e.printStackTrace();
 		}
     }
-	System.out.println(row);
-	System.out.println(column);
-	System.out.println(vehicles);
-	System.out.println(rides);
-	System.out.println(bonusPerRide);
-	System.out.println(numberOfSteps);
   }
 }
