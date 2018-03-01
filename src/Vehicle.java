e701b26ab8cf8a26a9d1f30d7e955c86b61fd328
John Hany <johnhany97@gmail.com>
2018-03-01 18:22:18 +0000

    Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,36 @@
 
 public class Vehicle {
+	private Ride ride;
+	private int row;
+	private int column;
+	
+	public Vehicle() {
+		this.row = 0;
+		this.column = 0;
+		this.ride = null;
+	}
 
+	public void setRide(Ride ride) {
+		this.ride = ride;
+	}
+	
+	public Ride getRide() {
+		return this.ride;
+	}
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	public int getColumn() {
+		return this.column;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public void setColumn(int column) {
+		this.column = column;
+	}
 }
